15d3b13636c23b6695e7971fb3f5d67ecf1d57fc
Berk Şengül <berksengul17@gmail.com>
2022-07-20 13:15:36 +0300

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,35 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let forwards = [];
+    let backwards = [];
+
+    for(let chr of str){
+        if(chr.toLowerCase() !== chr.toUpperCase()){
+            forwards.push(chr);
+        }
+    }
+
+    for(let i=str.length; i>=0; i--){
+        if(str.charAt(i).toLowerCase() !== str.charAt(i).toUpperCase()){
+            backwards.push(str.charAt(i));
+        }
+    }
+
+    return arraysMatch(forwards, backwards);
+
+};
+
+var arraysMatch = function (arr1, arr2) {
+
+	// Check if the arrays are the same length
+	if (arr1.length !== arr2.length) return false;
+
+	// Check if all items exist and are in the same order
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i].toLowerCase() !== arr2[i].toLowerCase()) return false;
+	}
+
+	// Otherwise, return true
+	return true;
 
 };
 

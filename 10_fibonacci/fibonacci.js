b8ab4b226495dcbce6982bae2b6adb0cb719282b
Berk Şengül <berksengul17@gmail.com>
2022-07-20 13:52:49 +0300

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    if(target < 0){
+        return "OOPS";
+    }
 
+    if(target == 1 || target == 2){
+        return 1;
+    }
+
+    let first = 1;
+    let second = 1;
+
+    for(let i=3; i<=target; i++){
+        temp = first;
+        first = second;
+        second += temp;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
